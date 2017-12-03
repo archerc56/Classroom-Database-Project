59116cc3b47a50fdeccfd18b92ac6760ddd403a9
Cade Archer <ccarcher14@gmail.com>
2017-12-03 09:39:46 -0600

    Update settings.py

--- classroom_project/settings.py
+++ classroom_project/settings.py
@@ -27,7 +27,7 @@ ECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ['archerClassroomProject.herokuapp.com']
+ALLOWED_HOSTS = ['classroomproject.herokuapp.com']
 
 
 # Application definition
